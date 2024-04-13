7e898a4949712e84efdce618d1c5d031710188b6
alekswtf <alekseywtfg@gmail.com>
2024-04-13 18:01:40 +0300

    add shop

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.card-button');
-
-button.addEventListener('click', () => {
-  if (button.textContent === 'Buy') {
-    button.textContent = 'Own';
-  } else {
-    button.textContent = 'Buy';
-  }
-});
